fc4ea87eb2df5dd32e0402775b81c84555abe1f5
lttofu <37090451+lttofu@users.noreply.github.com>
2020-10-17 15:50:56 -0400

    experiments & stuff

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-var _0xBitcoinToken = artifacts.require("./_0xBitcoinToken.sol");
+var _Dummy0xBTC = artifacts.require("./_Dummy0xBTC.sol");
+var _MintHelper = artifacts.require("./MintHelper.sol");
+//var _SkeleToken = artifacts.require("./ERC20Interface.sol");
+//var _Dummy0xBTC = artifacts.require("./ERC918Interface.sol");
+//var _SafeMath = artifacts.require("./SafeMath.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(_0xBitcoinToken);
+  deployer.deploy(_Dummy0xBTC);
+  deployer.deploy(_MintHelper);
 };
